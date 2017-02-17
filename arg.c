49b5d4f3d9d6bb598ad0a440e43db08fcc1394ee
Gang Zhuo <gang.zhuo@gmail.com>
2017-02-17 03:54:49 -0500

    update arg.c

--- arg.c
+++ arg.c
@@ -18,18 +18,6 @@ static inline char *findchar(char *str, int ch)
 	return p;
 }
 
-static inline char *arg_strdup(const char *str)
-{
-	char *res = 0;
-	if (str) {
-		res = (char *)pcs_malloc(strlen(str) + 1);
-		if (!res)
-			return 0;
-		strcpy(res, str);
-	}
-	return res;
-}
-
 /*从程序路径中找到文件名开始的位置，返回开始位置的指针*/
 static inline char *filename(char *path)
 {
@@ -43,14 +31,8 @@ static inline char *filename(char *path)
 
 #pragma region 解析参数
 
-/*用于释放struct arg中opts哈希表中的值字段*/
-static void free_opt(void *p)
-{
-	if (p) pcs_free(p);
-}
-
 /*解析参数*/
-int parse_arg(struct args *arg, int argc, char *argv[], char *(*s2utf8)(const char *s))
+int parse_args(struct args *arg, int argc, char *argv[])
 {
 	int i, j = 0;
 	char *p, *op, *val;
@@ -65,7 +47,7 @@ int parse_arg(struct args *arg, int argc, char *argv[], char *(*s2utf8)(const ch
 		}
 	}
 	if (arg->argc > 0) arg->argc--;
-	if (arg->optc > 0) arg->opts = ht_create((int)((float)arg->optc * HASH_EXTEND_MULTIPLIER), 0, &free_opt);
+	if (arg->optc > 0) arg->opts = ht_create((int)((float)arg->optc * HASH_EXTEND_MULTIPLIER), 0, NULL);
 	if (arg->argc > 0) arg->argv = (char **)pcs_malloc(arg->argc * sizeof(char *));
 	for (i = 1; i < argc; ++i) {
 		p = argv[i];
@@ -75,7 +57,7 @@ int parse_arg(struct args *arg, int argc, char *argv[], char *(*s2utf8)(const ch
 				p++;
 				val = findchar(p, '=');
 				if (ht_has(arg->opts, p, val - p)) return -1; /*重复指定参数*/
-				if (ht_add(arg->opts, p, val - p, (*val) == '=' ? (s2utf8 ? s2utf8(val + 1) : arg_strdup(val + 1)) : NULL))
+				if (ht_add(arg->opts, p, val - p, (*val) == '=' ? val + 1 : NULL))
 					return -1; /*添加到哈希表中失败*/
 			}
 			else {
@@ -92,7 +74,7 @@ int parse_arg(struct args *arg, int argc, char *argv[], char *(*s2utf8)(const ch
 			arg->cmd = p;
 		}
 		else {
-			arg->argv[j++] = s2utf8 ? s2utf8(p) : arg_strdup(p);
+			arg->argv[j++] = p;
 		}
 	}
 	return 0;
@@ -102,17 +84,10 @@ void free_args(struct args *arg)
 {
 	if (arg->opts) ht_destroy(arg->opts);
 	if (arg->argv) {
-		int i;
-		for (i = 0; i < arg->argc; i++) {
-			char *p = arg->argv[i];
-			if (p) pcs_free(p);
-		}
 		pcs_free(arg->argv);
 	}
 }
 
-/*判断是否存在opt选项
-如果存在的话，返回1,否则返回0*/
 int has_opt(struct args *arg, const char *opt)
 {
 	if (arg->opts) {
@@ -121,8 +96,6 @@ int has_opt(struct args *arg, const char *opt)
 	return 0;
 }
 
-/*判断是否存在opt选项，如果存在的话，把其值填入 （*pValue）中，并返回1，
-如果不存在的话，则设置 (*pValue) = NULL, 并返回0*/
 int has_optEx(struct args *arg, const char *opt, char **pValue)
 {
 	if (arg->opts) {
@@ -136,17 +109,28 @@ int has_optEx(struct args *arg, const char *opt, char **pValue)
 	return 0;
 }
 
-/*
-测试arg中可选项是否正确。
-  arg     -
-  ...     - 支持的所有可选项，以NULL结束
-如果测试通过则返回0，否则返回非零值
-例：
-  if (test_arg(arg, 0, 1, "d", "e", "r", "config", NULL)) {
-    printf("Wrong Arguments\n");
-    return -1;
-  }
-*/
+int has_vopts(struct args *arg, va_list ap)
+{
+	int co = 0;
+	const char *opt;
+	if (!arg->opts) return 0;
+	while ((opt = va_arg(ap, const char *)) != NULL) {
+		if (ht_has(arg->opts, opt, -1))
+			co++;
+	}
+	return co;
+}
+
+int has_opts(struct args *arg, ...)
+{
+	int rc = 0;
+	va_list ap;
+	va_start(ap, arg);
+	rc = has_vopts(arg, ap);
+	va_end(ap);
+	return rc;
+}
+
 int test_opts(struct args *arg, ...)
 {
 	int co = 0;
@@ -162,80 +146,6 @@ int test_opts(struct args *arg, ...)
 	return arg->opts->count - co;
 }
 
-/*
-删除一个选项。
-删除成功返回0，否则返回非0值。
-执行成功，且 pValue不为NULL，则把被删除项的值写入 (*pValue)。注意值需要调用pcs_free()
-*/
-int remove_opt(struct args *arg, const char *opt, char **pValue)
-{
-	char *val = NULL;
-	if (!arg->opts) return -1;
-	int rc = ht_remove(arg->opts, opt, -1, (void *)(pValue ? pValue : &val));
-	if (rc == 0) {
-		if (val) pcs_free(val);
-		arg->optc--;
-	}
-	return rc;
-}
-
-/*
-判断是否存在opt选项
-  ap - 待查询的所有可选项，以NULL结束
-如果全部不存在则返回0，否则返回有几个存在
-例：
-  if (has_vopt(arg, "d", "e", "r", "config", NULL)) {
-    printf("One of the options exists.\n");
-  } else {
-    printf("All of the options not exists.\n");
-  }
-*/
-int has_vopts(struct args *arg, va_list ap)
-{
-	int co = 0;
-	const char *opt;
-	if (!arg->opts) return 0;
-	while ((opt = va_arg(ap, const char *)) != NULL) {
-		if (ht_has(arg->opts, opt, -1))
-			co++;
-	}
-	return co;
-}
-
-/*
-判断是否存在opt选项
-  ap - 待查询的所有可选项，以NULL结束
-如果全部不存在则返回0，否则返回有几个存在
-例：
-  if (has_vopt(arg, "d", "e", "r", "config", NULL)) {
-    printf("One of the options exists.\n");
-  } else {
-    printf("All of the options not exists.\n");
-  }
-*/
-int has_opts(struct args *arg, ...)
-{
-	int rc = 0;
-	va_list ap;
-	va_start(ap, arg);
-	rc = has_vopts(arg, ap);
-	va_end(ap);
-	return rc;
-}
-
-/*
-测试arg是否正确。
-arg     -
-minArgc - 允许的最少参数
-maxArgc - 允许的最多参数
-ap      - 支持的所有可选项，以NULL结束
-如果测试通过则返回0，否则返回非零值
-例：
-  if (test_arg(arg, 0, 1, "d", "e", "r", "config", NULL)) {
-    printf("Wrong Arguments\n");
-    return -1;
-  }
-*/
 int test_varg(struct args *arg, int minArgc, int maxArgc, va_list ap)
 {
 	int co = 0;
@@ -249,19 +159,6 @@ int test_varg(struct args *arg, int minArgc, int maxArgc, va_list ap)
 	return arg->opts->count - co;
 }
 
-/*
-测试arg是否正确。
- arg     -
- minArgc - 允许的最少参数
- maxArgc - 允许的最多参数
- ...     - 支持的所有可选项，以NULL结束
-如果测试通过则返回0，否则返回非零值
-例：
-   if (test_arg(arg, 0, 1, "d", "e", "r", "config", NULL)) {
-        printf("Wrong Arguments\n");
-		return -1;
-   }
-*/
 int test_arg(struct args *arg, int minArgc, int maxArgc, ...)
 {
 	int rc = 0;
@@ -269,6 +166,16 @@ int test_arg(struct args *arg, int minArgc, int maxArgc, ...)
 	va_start(ap, maxArgc);
 	rc = test_varg(arg, minArgc, maxArgc, ap);
 	va_end(ap);
+	return rc;
+}
+
+int remove_opt(struct args *arg, const char *opt, char **pValue)
+{
+	if (!arg->opts) return -1;
+	int rc = ht_remove(arg->opts, opt, -1, (void **)pValue);
+	if (rc == 0) {
+		arg->optc--;
+	}
 	return rc;
 }
 
--- arg.h
+++ arg.h
@@ -15,71 +15,59 @@ struct args
 };
 
 /*解析参数*/
-int parse_arg(struct args *arg, int argc, char *argv[], char *(*utf8)(const char *s));
-void free_args(struct args *arg);
-/*
-删除一个选项。
-删除成功返回0，否则返回非0值。
-执行成功，且 pValue不为NULL，则把被删除项的值写入 (*pValue)。注意值需要调用pcs_free()
-*/
-int remove_opt(struct args *arg, const char *opt, char **pValue);
+int parse_args(struct args *arg, int argc, char *argv[]);
 
-/*判断是否存在opt选项
-如果存在的话，返回1,否则返回0*/
+void free_args(struct args *arg);
+
+/*判断是否存在 opt 选项
+如果存在的话，返回 1, 否则返回 0 */
 int has_opt(struct args *arg, const char *opt);
 
-/*判断是否存在opt选项，如果存在的话，把其值填入 （*pValue）中，并返回1，
-如果不存在的话，则设置 (*pValue) = NULL, 并返回0*/
+/*判断是否存在 opt 选项，如果存在的话，把其值填入 （*pValue）中，并返回 1，
+如果不存在的话，则设置 (*pValue) = NULL, 并返回 0*/
 int has_optEx(struct args *arg, const char *opt, char **pValue);
 
+/* See int has_opts(struct args *arg, ...); */
+int has_vopts(struct args *arg, va_list ap);
+
 /*
-测试arg中可选项是否正确。
-arg     -
-...     - 支持的所有可选项，以NULL结束
-如果测试通过则返回0，否则返回非零值
+判断是否存在指定的 opt 选项
+  ap - 待查询的所有可选项，以 NULL 结束
+如果全部不存在则返回 0，否则返回存在的选项数量。
 例：
-  if (test_arg(arg, 0, 1, "d", "e", "r", "config", NULL)) {
+  if (has_vopt(arg, "d", "e", "r", "config", NULL)) {
+    printf("Exists one or more options.\n");
+  } else {
+    printf("No options exists.\n");
+  }
+*/
+int has_opts(struct args *arg, ...);
+
+/*
+测试可选项是否匹配。
+  arg     -
+  ...     - 匹配所有可选项，以NULL结束
+如果可选项完全匹配的话，则返回 0。
+如果传入的可选项比需要的多，返回 > 0 的值。
+如果传入的可选项比需要的少，返回 < 0 的值。
+例：
+  if (test_opts(arg, "d", "e", "r", "config", NULL)) {
     printf("Wrong Arguments\n");
     return -1;
   }
 */
 int test_opts(struct args *arg, ...);
 
-/*
-判断是否存在opt选项
-  ap - 待查询的所有可选项，以NULL结束
-如果全部不存在则返回0，否则返回有几个存在
-例：
-  if (has_vopt(arg, "d", "e", "r", "config", NULL)) {
-    printf("One of the options exists.\n");
-  } else {
-    printf("All of the options not exists.\n");
-  }
-*/
-int has_opts(struct args *arg, ...);
-
-/*
-测试arg是否正确。
-arg     -
-minArgc - 允许的最少参数
-maxArgc - 允许的最多参数
-ap      - 支持的所有可选项，以NULL结束
-如果测试通过则返回0，否则返回非零值
-例：
-  if (test_arg(arg, 0, 1, "d", "e", "r", "config", NULL)) {
-    printf("Wrong Arguments\n");
-    return -1;
-  }
-*/
+/* See int test_arg(struct args *arg, int minArgc, int maxArgc, ...); */
 int test_varg(struct args *arg, int minArgc, int maxArgc, va_list ap);
 
 /*
-测试arg是否正确。
+测试参数是否正确。
 arg     -
 minArgc - 允许的最少参数
 maxArgc - 允许的最多参数
-...     - 支持的所有可选项，以NULL结束
-如果测试通过则返回0，否则返回非零值
+ap      - 支持的所有可选项，以 NULL 结束
+如果测试通过则返回 0，否则返回非零值。
 例：
   if (test_arg(arg, 0, 1, "d", "e", "r", "config", NULL)) {
     printf("Wrong Arguments\n");
@@ -87,6 +75,13 @@ maxArgc - 允许的最多参数
   }
 */
 int test_arg(struct args *arg, int minArgc, int maxArgc, ...);
+
+/*
+删除一个选项。
+删除成功返回 0，否则返回非 0 值。
+执行成功，且 pValue 不为 NULL，则把被删除项的值写入 (*pValue)。
+*/
+int remove_opt(struct args *arg, const char *opt, char **pValue);
 
 
 #endif
